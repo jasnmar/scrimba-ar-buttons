a239be486a0b6a20bb85258f94c97dab432b7c29
jasnmar <jasnmar@gmail.com>
2024-04-24 12:52:45 -0400

    Large and Small button styles

--- src/component/Button/Button.jsx
+++ src/component/Button/Button.jsx
@@ -1,10 +1,35 @@
 import "./Button.css"
 
 function Button(props) {
-    console.log('props:', props)
+    console.log('props: ', props)
+
+        /**
+     * Challenge: accept the `size` prop and set the `className` of the
+     * <button> to:
+     * 
+     * "button-small" if `size` is "sm"
+     * "button-large" if `size` is "lg"
+     * 
+     * Note: don't try to manually add a `className` to the Button
+     * instance in index.js yet. (DO add `className` to this file
+     * as part of the challenge, though.)
+     */
+    const btnClass = getBtnClass()
+    function getBtnClass() {
+      let cName
+      if(props.size === "lg") {
+        cName = "button-large"
+      } else if(props.size === "sm") {
+        cName = "button-small"
+      }
+      return cName
+    }
+
+
+
     return (
         <>
-            <button {...props }>{props.children}</button>
+            <button className={btnClass} {...props }>{props.children}</button>
         </>
     )
 }
